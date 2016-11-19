b37f8535205215868653c9d1ce9dd1dd1496411e
wanjochan <wanjochan@gmail.com>
2016-11-19 11:15:37 +0800

    WJC: cook

--- scripts/PluginInstall_android.js
+++ scripts/PluginInstall_android.js
@@ -5,13 +5,8 @@ function sync_copy(src,tgt){
 	console.log('to copy '+src+' => '+tgt);
 	return fs.writeFileSync(tgt, fs.readFileSync(src));
 }
+
 module.exports = function(ctx) {
-	console.log(" before_plugin_add ctx ", ctx);
-
-	console.log("process.args");
-	console.log(process.argv);
-
-
 	// make sure android platform is part of build
 	//if (ctx.opts.platforms.indexOf('android') < 0) {
 	//	return;
@@ -19,8 +14,87 @@ module.exports = function(ctx) {
 
 	fs = ctx.requireCordovaModule('fs');
 	var path = ctx.requireCordovaModule('path');
-	var deferral = ctx.requireCordovaModule('q').defer();
+	var Q=ctx.requireCordovaModule('q');
 
+	var defer= Q.defer();
+
+	var pluginRoot =ctx.opts.plugin.dir;
+	var projectRoot=ctx.opts.projectRoot;
+	var platformRoot = path.join(projectRoot, 'platforms/android');
+	
+	function copyFiles(srcPath, destPath) {
+		console.log(" copyFiles() ",srcPath," => ",destPath);
+		if (fs.statSync(srcPath).isDirectory()) {
+			if (!fs.existsSync(destPath)) {
+				fs.mkdirSync(destPath);
+			}
+			fs.readdirSync(srcPath).forEach(function (child) {
+				copyFiles(path.join(srcPath, child), path.join(destPath, child));
+			});
+		} else {
+			fs.writeFileSync(destPath, fs.readFileSync(srcPath));
+		}
+	}  
+	
+	/*
+	return 
+	Q("what").then //OK
+	//Q.try//OK
+	//Q.fcall//OK
+	return Q.try
+	Q().then
+	 */
+	//var rt=Q.try
+
+	var exec = Q.nfbind(require('child_process').exec);//bind-node-to-Q
+	var spawn = Q.nfbind(require('child_process').spawn);//bind-node-to-Q
+	
+	//return
+	Q.try(function(){
+		console.log(" ---- start plugin install ----");
+		//console.log("ctx=",ctx);
+	})
+		.then(function(){
+			return exec("cd "+pluginRoot +" && ls -al").then(function(stdout,stderr){
+				if(stdout) console.log("stdout=",stdout.join("\n"));
+				if(stderr) console.log("stderr=",stderr.join("\n"));
+			});
+		})
+		.then(function(){
+			console.log("try git update libs");
+			//git -C lib/lib-ios-jso pull || git clone https://github.com/SZU-BDI/lib-ios-jso.git lib/lib-ios-jso"
+			return exec("cd "+projectRoot
+				+" && pwd && (git -C lib/app-hybrid-core pull || git clone https://github.com/SZU-BDI/app-hybrid-core.git --depth=1 --branch master --single-branch lib/app-hybrid-core) && ls -al")
+				.then(function(stdout,stderr){
+					if(stdout) console.log("stdout=",stdout.join("\n"));
+					if(stderr) console.log("stderr=",stderr.join("\n"));
+				});
+		}).then(function(prev){
+			console.log("sync files after git update ");
+			return Q.try(function(){
+				copyFiles(projectRoot+"/lib/app-hybrid-core/lib-android/szu.bdi.hybrid.core/src/main/java",platformRoot+"/src");
+			});
+		}).catch(function(ex){
+			console.log("some error ",ex);
+			defer.reject(ex);
+			return defer.promise;
+		}).finally(function(p1){
+			if(p1){
+				console.log("p1=",p1);
+			}
+			console.log("finally do sth...");
+			return defer.promise;
+		})/*.done(function(p1,p2,p3){
+		if(p1 || p2 || p3) console.log("done ",p1,p2,p3);
+		else console.log("done()");
+
+		defer.resolve("install plugin finished");
+	})*/
+	;
+	//console.log("rt",rt);
+
+	return defer.promise;
+	/*
 	//console.log(" process.cwd() ", process.cwd());
 	//console.log(" opts.plugin ", ctx.opts.plugin);
 	return deferral.promise;
@@ -48,6 +122,7 @@ module.exports = function(ctx) {
 	//});
 
 	return deferral.promise;
+	*/
 };
 /*
  *
